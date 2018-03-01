b6ef649080a9308fc17dc71dad3b03ead0607e62
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-01 08:58:06 -0500

    NOP minor cleanup of files.go

--- file/file.go
+++ file/file.go
@@ -1,5 +1,8 @@
 // Doost
 
+// Package file has mostly helper functions dealing with files.
+// Specific gart system files are handled in their respective packages
+// e.g. gart/index/card.
 package file
 
 import (
@@ -9,8 +12,10 @@ import (
 	"syscall"
 )
 
-func CheckFile(fname string) {}
+/// types /////////////////////////////////////////////////////////////////////
 
+// Details encapsulates the necessary details for gart purposes.
+// This structure is typically provided for user files.
 type Details struct {
 	Path   string // absolute path
 	Dir    string // from root dir
@@ -20,27 +25,12 @@ type Details struct {
 	Statfs syscall.Statfs_t
 }
 
+// REVU TODO filesystem details would be nice.
+//			 also consider a Print func for full emit of details on multi-lines.
+// String returns representation of Details suitable for logging.
 func (s Details) String() string {
 	str := fmt.Sprintf("file.Details name:%s path:%s dir:%s ext:%s", s.Name, s.Path, s.Dir, s.Ext)
 	return str
-}
-
-func getAbsolutePath(name string) (string, error) {
-	if filepath.IsAbs(name) {
-		return name, nil
-	}
-
-	wd, e := os.Getwd()
-	if e != nil {
-		return "", e
-	}
-
-	fpath, e := filepath.Abs(filepath.Join(wd, name))
-	if e != nil {
-		return "", e
-	}
-
-	return fpath, nil
 }
 
 func GetDetails(name string) (Details, error) {
@@ -71,4 +61,24 @@ func GetDetails(name string) (Details, error) {
 	}
 
 	return details, nil
+}
+
+/// general helper functions //////////////////////////////////////////////////
+
+func getAbsolutePath(name string) (string, error) {
+	if filepath.IsAbs(name) {
+		return name, nil
+	}
+
+	wd, e := os.Getwd()
+	if e != nil {
+		return "", e
+	}
+
+	fpath, e := filepath.Abs(filepath.Join(wd, name))
+	if e != nil {
+		return "", e
+	}
+
+	return fpath, nil
 }
