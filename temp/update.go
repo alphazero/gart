ca678515a3e973a3f16ff504c199abc7abc965d4
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-01 23:17:36 -0500

    MOD binary var-len
       ISSUE: 18MB read/write takes ~1.3 seconds for 131072 records
       TODO: seek a specific record and update its refcnt

--- temp/update.go
+++ temp/update.go
@@ -19,9 +19,10 @@ var random = rand.New(rand.NewSource(0))
 
 // Record is variable length and persisted with \n record delim.
 type Record struct {
+	flags  byte
 	id     uint32
-	refcnt uint32 // REVU use unum?
-	val    string
+	refcnt uint32
+	val    string // REVU TODO must insure that the byte len is < 256
 }
 
 func (Record) randomRecord(maxValLen int) Record {
@@ -33,16 +34,104 @@ func (Record) randomRecord(maxValLen int) Record {
 	// for now, just write valLen 'v'
 	val := make([]byte, valLen)
 	for i := range val {
-		val[i] = 'v'
+		if random.Intn(10) == 0 {
+			val[i] = ' '
+		} else if random.Intn(20) == 0 {
+			val[i] = '-'
+		} else {
+			val[i] = 'v'
+		}
 	}
-	return Record{0, 0, string(val)}
+	return Record{0x00, 0, 0, string(val)}
+}
+
+func (r Record) String() string {
+	return fmt.Sprintf("%08b %d %d %q", r.flags, r.id, r.refcnt, r.val)
+}
+
+func (r Record) Encode(w io.Writer) (int, error) {
+	var wlen int
+
+	var aflag = [1]byte{r.flags}
+	if n, e := w.Write(aflag[:]); e != nil || n < 1 {
+		return n, fmt.Errorf("Record.Encode - flag - wrote %d bytes with error %v", n, e)
+	} else {
+		wlen += n
+	}
+	var apid = *(*[4]byte)(unsafe.Pointer(&(r.id)))
+	if n, e := w.Write(apid[:]); e != nil || n < 4 {
+		return n, fmt.Errorf("Record.Encode - pid- wrote %d bytes with error %v", n, e)
+	} else {
+		wlen += n
+	}
+	var arefcnt = *(*[4]byte)(unsafe.Pointer(&(r.refcnt)))
+	if n, e := w.Write(arefcnt[:]); e != nil || n < 4 {
+		return n, fmt.Errorf("Record.Encode - refcnt- wrote %d bytes with error %v", n, e)
+	} else {
+		wlen += n
+	}
+	var vlen = [1]byte{byte(len(r.val))} // REVU insure vlen is always less than 256? TODO ..
+	if n, e := w.Write(vlen[:]); e != nil || n < 1 {
+		return n, fmt.Errorf("Record.Encode - vlen - wrote %d bytes with error %v", n, e)
+	} else {
+		wlen += n
+	}
+	if n, e := w.Write([]byte(r.val)); e != nil || n < len(r.val) {
+		return n, fmt.Errorf("Record.Encode - val - wrote %d bytes with error %v", n, e)
+	} else {
+		wlen += n
+	}
+	return wlen, nil
+}
+
+func (p *Record) Decode(r io.Reader) (int, error) {
+	var rlen int
+
+	var aflags [1]byte
+	if n, e := r.Read(aflags[:]); e != nil || n < 1 {
+		return n, fmt.Errorf("Record.Decode - flag read %d bytes with error %v", n, e)
+	} else {
+		rlen += n
+	}
+	var apid [4]byte
+	if n, e := r.Read(apid[:]); e != nil || n < 4 {
+		return rlen + n, fmt.Errorf("Record.Decode - pid read %d bytes with error %v", n, e)
+	} else {
+		rlen += n
+	}
+	var arefcnt [4]byte
+	if n, e := r.Read(arefcnt[:]); e != nil || n < 4 {
+		return rlen + n, fmt.Errorf("Record.Decode - refcnt read %d bytes with error %v", n, e)
+	} else {
+		rlen += n
+	}
+	var avlen [1]byte
+	if n, e := r.Read(avlen[:]); e != nil || n < 1 {
+		return rlen + n, fmt.Errorf("Record.Decode - vlen read %d bytes with error %v", n, e)
+	} else {
+		rlen += n
+	}
+	vlen := int(avlen[0])
+	//	fmt.Printf("debug - vlen %d\n", vlen)
+	var val = make([]byte, vlen)
+	if n, e := r.Read(val[:]); e != nil || n < vlen {
+		//		println(len(val))
+		return rlen + n, fmt.Errorf("Record.Decode - val read %d bytes of %d with error %v", n, vlen, e)
+	} else {
+		rlen += n
+	}
+	p.flags = aflags[0]
+	p.id = *(*uint32)(unsafe.Pointer(&apid[0]))
+	p.refcnt = *(*uint32)(unsafe.Pointer(&arefcnt[0]))
+	p.val = string(val)
+	return rlen, nil
 }
 
 /// test //////////////////////////////////////////////////////////////////////
 
 func main() {
 
-	var items = 80
+	var items = 1 << 17
 	const fname = "tempfile"
 
 	// create file, write rand records, and close
@@ -52,7 +141,7 @@ func main() {
 		fmt.Printf("wrote %d records\n", n)
 	}
 	if n, e := verifyFileWrite(fname, items); e != nil {
-		exitOnError(fmt.Errorf("err - wrote %d records - error %v", n, e))
+		exitOnError(fmt.Errorf("read %d records - error %v", n, e))
 	} else {
 		fmt.Printf("verified %d records\n", n)
 	}
@@ -64,54 +153,61 @@ var E_IllegalArgument = fmt.Errorf("err - illegal argument")
 var recterm = []byte("\n")
 
 func verifyFileWrite(fname string, items int) (int, error) {
-	return items, nil
-}
 
-func writeRandomFile(fname string, items int) (int, error) {
-	//	const flags = os.O_CREATE | os.O_EXCL | os.O_APPEND | os.O_SYNC // REVU is O_SYNC necessary?
-	const flags = os.O_CREATE | os.O_EXCL | os.O_WRONLY | os.O_SYNC | os.O_APPEND // | os.O_SYNC // REVU is O_SYNC necessary?
+	const flags = os.O_RDONLY
+	fi, e := os.Stat(fname)
+	if e != nil {
+		panic(e)
+	}
+
 	file, e := os.OpenFile(fname, flags, 0644)
 	if e != nil {
 		return 0, e
 	}
 	defer file.Close()
-	defer file.Sync()
+
+	var tot int
+	var r = bufio.NewReaderSize(file, int(fi.Size()))
+	for i := 0; i < items; i++ {
+		var record Record
+		n, e := record.Decode(r)
+		//n, e := record.Decode(file)
+		if e != nil {
+			return i, e
+		}
+		tot += n
+		//		fmt.Printf("%s\n", record)
+		//		fmt.Printf("tot: %d\n", tot)
+	}
+	return items, nil
+}
+
+func writeRandomFile(fname string, items int) (int, error) {
+	const flags = os.O_CREATE | os.O_EXCL | os.O_WRONLY | os.O_APPEND //REVU is O_SYNC necessary?
+	file, e := os.OpenFile(fname, flags, 0644)
+	if e != nil {
+		return 0, e
+	}
+	defer file.Close()
+	//	defer file.Sync()
 
 	var w = bufio.NewWriter(file)
 	defer w.Flush()
 
 	for i := 0; i < items; i++ {
 		var record Record
-		record = record.randomRecord(64)
+		record = record.randomRecord(256)
 		record.id = uint32(i)
-
-		pid := (*[4]byte)(unsafe.Pointer(&record.id))
-		refcnt := (*[4]byte)(unsafe.Pointer(&record.refcnt))
-		sbuf := []byte(record.val)
-		var e error
-		e = verifyWrite(e, w, (*pid)[:], 4)
-		e = verifyWrite(e, w, (*refcnt)[:], 4)
-		e = verifyWrite(e, w, sbuf, len(sbuf))
-		e = verifyWrite(e, w, recterm, 1)
-		if e != nil {
+		record.refcnt = 1
+		record.flags = 0xff
+		if _, e := record.Encode(w); e != nil {
 			return i, e
+		}
+		if e := file.Sync(); e != nil {
+			panic(e)
 		}
 	}
 	return items, nil
-}
-
-func verifyWrite(e error, w io.Writer, b []byte, n int) error {
-	if e != nil {
-		return e
-	}
-	n0, e0 := w.Write(b)
-	if e0 != nil {
-		return e0
-	}
-	if n0 != n {
-		return fmt.Errorf("err - verifyWrite - expected %d  - wrote %d", n, n0)
-	}
-	return nil
 }
 
 /// helpers ///////////////////////////////////////////////////////////////////
