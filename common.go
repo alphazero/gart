180fbf76a82229f757409fa059bde5a0b1b31451
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-07 14:55:11 -0500

    MOD TESTED initGart - create TAGS

--- common.go
+++ common.go
@@ -3,9 +3,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/alphazero/gart/fs"
+	"github.com/alphazero/gart/tag"
 )
 
 /// system wide consts & vars /////////////////////////////////////////////////
@@ -29,9 +32,9 @@ var gartDirs = []string{
 
 // gart metadata and index files
 const (
-	tagsdefFile  = "tags/tagsdef"
-	tagIndexFile = "index/tags.idx"
-	devicesFile  = "path/devices"
+	tagsdefFile  = "tags/TAGS"
+	tagIndexFile = "index/INDEX"
+	devicesFile  = "path/DEVICES"
 )
 
 /// initialization & process boot /////////////////////////////////////////////
@@ -52,6 +55,34 @@ func initOrVerifyGart(pi processInfo) error {
 
 }
 
+// panics if init is called for an already initialized gart (REVU for now).
+// Errors should be considered fail-stop.
+func initGart(pi processInfo) error {
+	if e := fs.WalkDirs(pi.user.HomeDir, gartDirs, func(path string) error {
+		return os.Mkdir(path, fs.DirPerm)
+	}); e != nil {
+		panic(e)
+	}
+
+	var fname string
+	// TODO create minimal/initial gart files
+
+	// tag definitions file
+	fname = filepath.Join(pi.gartDir, tagsdefFile)
+	_, e := tag.LoadMap(fname, true)
+	if e != nil {
+		return e
+	}
+	// verify
+	if e := fs.VerifyFile(fname); e != nil {
+		panic(fmt.Errorf("bug - initGart: verification of %s failed - err: %v", fname, e))
+	}
+
+	// index
+	// etc
+	panic("initGart - is incomplete")
+}
+
 func verifyGartRepo(pi processInfo) error {
 	// verify directory structure
 	if e := fs.WalkDirs(pi.user.HomeDir, gartDirs, fs.VerifyDir); e != nil {
@@ -62,20 +93,4 @@ func verifyGartRepo(pi processInfo) error {
 	// .gart/tag/tags
 	// .gart/path/devices
 	panic("verifyGartRepo - is imcomplete")
-}
-
-// panics if init is called for an already initialized gart (REVU for now).
-// Errors should be considered fail-stop.
-func initGart(pi processInfo) error {
-	if e := fs.WalkDirs(pi.user.HomeDir, gartDirs, func(path string) error {
-		return os.Mkdir(path, fs.DirPerm)
-	}); e != nil {
-		panic(e)
-	}
-
-	// TODO create minimal/initial gart files
-	// tagsdef
-	// index
-	// etc
-	panic("initGart - is incomplete")
 }
