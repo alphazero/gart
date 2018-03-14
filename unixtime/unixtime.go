0c19c0814420469a7bf8e645016cc43940b40423
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-14 08:49:13 -0400

    MOD rename Epoch to Time

--- unixtime/unixtime.go
+++ unixtime/unixtime.go
@@ -10,14 +10,14 @@ import (
 	"time"
 )
 
-// UnixEpoch just holds the unix seconds.
-type Epoch uint32
+// Time just holds the unix seconds.
+type Time uint32
 
-// Returns the std-lib time.Time for this unixtime.Epoch.
-func (t Epoch) Time() time.Time { return time.Unix(int64(t), 0) }
+// Returns the std-lib time.Time for this unixtime.Time.
+func (t Time) Time() time.Time { return time.Unix(int64(t), 0) }
 
 // Returns the mmm-dd-yyyy (ex: mar-21-2018)
-func (t Epoch) Date() string {
+func (t Time) Date() string {
 	y, m, d := t.Time().Date()
 	return fmt.Sprintf("%s-%02d-%4d", strings.ToLower(m.String()[:3]), d, y)
 }
@@ -28,8 +28,8 @@ func timenow() time.Time {
 	return time.Now()
 }
 
-// Returns unix Epoch for time.Now()
-func Now() (Epoch, time.Time) {
+// Returns unix Time for time.Now()
+func Now() (Time, time.Time) {
 	now := timenow()
-	return Epoch(uint32(now.Unix()) & 0xffffffff), now
+	return Time(uint32(now.Unix()) & 0xffffffff), now
 }
