08efb3a9c375f24a9042b9fe90dc4e98653c9d02
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-21 11:04:57 -0400

    ADD syslib/sort package (for []uint64)

---
+++ syslib/sort/sort_test.go
@@ -0,0 +1,18 @@
+// Doost!
+package sort_test
+
+import (
+	"github.com/alphazero/gart/syslib/sort"
+	"testing"
+)
+
+func TestUint64Slice(t *testing.T) {
+	var arr = []uint64{34, 57, 17, 18, 27}
+	var exp = []uint64{17, 18, 27, 34, 57}
+	sort.Uint64(arr)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != exp[i] {
+			t.Errorf("have: %d - expect: %d", arr[i], exp[i])
+		}
+	}
+}
