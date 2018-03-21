c1c5707b0d862445ce5c0e38a5785c080f079d62
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-21 09:50:03 -0400

    ADD doc for package system.

---
+++ system/doc.go
@@ -0,0 +1,9 @@
+// Doost!
+
+// package system defines the system wide consts and vars; standard errors, and
+// standard error formatting; nominal repository structure and paths; and system
+// wide types.
+//
+// Additionally, system boot-up initialization and runtime context are defined
+// and initialized in this package.
+package system
