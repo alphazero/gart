485b28f73b2af50f9de1c04885dad917c9854b8b
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-23 18:02:48 -0400

    ADD TESTED sort for []uint

--- syslib/sort/sort.go
+++ syslib/sort/sort.go
@@ -9,13 +9,24 @@ import (
 // shamelessly copied from the std-lib sort and modified for our
 // favored types.
 
-func Uint64(a []uint64) { stdlib.Sort(Uint64Slice(a)) }
+/// uint64 /////////////////////////////////////////////////////////////////////
 
 type Uint64Slice []uint64
+
+func Uint64s(a []uint64) { stdlib.Sort(Uint64Slice(a)) }
 
 func (p Uint64Slice) Len() int           { return len(p) }
 func (p Uint64Slice) Less(i, j int) bool { return p[i] < p[j] }
 func (p Uint64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p Uint64Slice) Sort()              { stdlib.Sort(p) }
 
-// Sort is a convenience method.
-func (p Uint64Slice) Sort() { stdlib.Sort(p) }
+/// uint ///////////////////////////////////////////////////////////////////////
+
+type UintSlice []uint
+
+func Uints(a []uint) { stdlib.Sort(UintSlice(a)) }
+
+func (p UintSlice) Len() int           { return len(p) }
+func (p UintSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p UintSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p UintSlice) Sort()              { stdlib.Sort(p) }
--- syslib/sort/sort_test.go
+++ syslib/sort/sort_test.go
@@ -7,10 +7,21 @@ import (
 	"github.com/alphazero/gart/syslib/sort"
 )
 
+func TestUintSlice(t *testing.T) {
+	var arr = []uint{34, 57, 17, 18, 27}
+	var exp = []uint{17, 18, 27, 34, 57}
+	sort.Uints(arr)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != exp[i] {
+			t.Errorf("have: %d - expect: %d", arr[i], exp[i])
+		}
+	}
+}
+
 func TestUint64Slice(t *testing.T) {
 	var arr = []uint64{34, 57, 17, 18, 27}
 	var exp = []uint64{17, 18, 27, 34, 57}
-	sort.Uint64(arr)
+	sort.Uint64s(arr)
 	for i := 0; i < len(arr); i++ {
 		if arr[i] != exp[i] {
 			t.Errorf("have: %d - expect: %d", arr[i], exp[i])
