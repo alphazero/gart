9eb07df17b7b662c2d2991c0e7774f260a23e0d5
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-26 13:32:47 -0400

    ADD WIP basic mappedFile create, open, and read.

--- syslib/bitmap/tagmap/tagmaps.go
+++ syslib/bitmap/tagmap/tagmaps.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	//	"syscall"
+	"syscall"
 	"time"
 	"unsafe"
 
@@ -31,19 +31,80 @@ import (
 // - tagmap manager
 //		- AND 2 or more tagmaps and get 'keyset'
 
-/// tagmaps prototype //////////////////////////////////////////////////////////
+/// op mode ///////////////////////////////////////////////////////////////////
 
-const headerSize = 512
+// REVU this is exactly the same thing as op mode for index.
+// can't be in gart/index as index package will include the object and
+// tagmap sub-packages.  TODO think about this ..
+type OpMode byte
 
-type header struct {
-	ftype    uint64
-	crc64    uint64
-	created  int64
-	updated  int64
-	reserved [480]byte
+const (
+	Read OpMode = 1 << iota
+	Write
+	Verify
+	Compact
+)
+
+// panics on unimplemented op mode
+func (m OpMode) fopenFlag() int {
+	switch m {
+	case Read:
+		return os.O_RDONLY
+	case Write:
+		return os.O_RDWR
+	case Verify:
+	case Compact:
+	default:
+	}
+	panic(errors.NotImplemented("tagmap.OpMode: not implemented - mode  %d", m))
 }
 
-func (h header) encode(buf []byte) error {
+// panics on invalid opMode
+func (m OpMode) verify() error {
+	switch m {
+	case Read:
+	case Write:
+	case Verify:
+	case Compact:
+	default:
+		return errors.Bug("tagmap.OpMode: unknown mode - %d", m)
+	}
+	return nil
+}
+
+// Returns string rep. of opMode
+func (m OpMode) String() string {
+	switch m {
+	case Read:
+		return "Read"
+	case Write:
+		return "Write"
+	case Verify:
+		return "Verify"
+	case Compact:
+		return "Compact"
+	}
+	panic(errors.Bug("tagmap.OpMode: unknown mode - %d", m))
+}
+
+/// tagmap file header /////////////////////////////////////////////////////////
+
+const headerSize = 32
+const mmap_tagmap_ftype = 0x5807263e43839459
+
+// tagmap header is the minimal content of a valid gart tagmap file.
+type header struct {
+	ftype   uint64
+	crc64   uint64
+	created int64  // unix nanos
+	updated int64  // unix nanos
+	mapSize uint64 // bytes - number of blocks * 4
+	mapMax  uint64 // max bitnum in bitmap
+}
+
+// encode writes the header data to the given buffer.
+// Returns error if buf length < tagmap.headerSize.
+func (h *header) encode(buf []byte) error {
 	if len(buf) < headerSize {
 		return errors.Error("header.encode: insufficient buffer length: %d", len(buf))
 	}
@@ -55,9 +116,39 @@ func (h header) encode(buf []byte) error {
 	*(*uint64)(unsafe.Pointer(&buf[8])) = h.crc64
 	return nil
 }
+func (h *header) decode(buf []byte) error {
+	if len(buf) < headerSize {
+		return errors.Error("header.decode: insufficient buffer length: %d", len(buf))
+	}
+	*h = *(*header)(unsafe.Pointer(&buf[0]))
 
-// Returns the absolute path filename for the given tag.
-func TagFilename(tag string) string {
+	// verify
+	// ftype
+	if h.ftype != mmap_tagmap_ftype {
+		errors.Bug("header.decode: invalid ftype: %x - expect: %x",
+			h.ftype, mmap_tagmap_ftype)
+	}
+	// TODO created, updated can be also checked.
+	// crc
+	crc64 := digest.Checksum64(buf[16:])
+	if crc64 != h.crc64 {
+		errors.Bug("header.decode: invalid checksum: %d - expect: %d",
+			h.crc64, crc64)
+	}
+
+	return nil
+}
+
+/// tagmap file ////////////////////////////////////////////////////////////////
+
+// Returns the absolute path filename for the given tag. The full path is a
+// variation on git's approach to blob file paths: tag name is converted to
+// lower-case form; (b) the blake2b uint64 hash of that is used to construct
+// the path fragment /xx/xxxxxxxxxxxx. We only use 8 bytes since that is more
+// than sufficient given that the total number of tags in gart will be far less
+// than 2^32.
+func TagmapFilename(tag string) string {
+	tag = strings.ToLower(tag)
 	hash := fmt.Sprintf("%x.bitmap", digest.SumUint64([]byte(tag)))
 	path := filepath.Join(system.IndexTagmapsPath, hash[:2])
 	return filepath.Join(path, hash[2:])
@@ -68,8 +159,9 @@ func TagFilename(tag string) string {
 // (name) will always be converted to lower-case form.
 func CreateTagmap(tag string) error {
 
-	tag = strings.ToLower(tag)
-	filename := TagFilename(tag)
+	filename := TagmapFilename(tag)
+
+	// if dir structure does not exist, create it.
 	dir := filepath.Dir(filename)
 	if e := os.MkdirAll(dir, system.DirPerm); e != nil {
 		return errors.ErrorWithCause(e, "CreateTagmap: dir: %q", dir)
@@ -83,7 +175,7 @@ func CreateTagmap(tag string) error {
 
 	var now = time.Now().UnixNano()
 	var h = &header{
-		ftype:   0x5807263e43839459,
+		ftype:   mmap_tagmap_ftype,
 		created: now,
 		updated: now,
 	}
@@ -97,8 +189,162 @@ func CreateTagmap(tag string) error {
 	if e != nil {
 		return errors.ErrorWithCause(e, "createTagmap: tag: %s", tag)
 	}
+	return nil
+}
 
-	return errors.NotImplemented("createTagmap - filename: %s", filename)
+// tagmap use-cases:
+//
+//	- Query: given one or more tags, get all object keys. N Reads.
+//	- Read: [dev] simply get the wahl bitmap for a given tag. 1 Read.
+//  - Update: set/clear bit(s) for a given tag. N updates.
+//	  this is a swap file mode. REVU the entire point of this adhoc exercise
+//	  was to find out what is the best way to update the tagmap files!
+//  - Compact: See update.
+//
+// REVU both tagmap and object-index (mmap) share the same OpMode and open
+// 		sequence. TODO think about consolidation (later).
+//
+func OpenTagmap(tag string, opMode OpMode) (*mappedFile, error) {
+
+	return OpenMappedFile(TagmapFilename(tag), opMode)
+}
+
+/// memory mapped file /////////////////////////////////////////////////////////
+
+// REVU good idea to TODO move this to fs package and use same in object-index.
+type mappedFile struct {
+	*header
+	filename string
+	opMode   OpMode
+	file     *os.File
+	finfo    os.FileInfo // size is int64
+	flags    int
+	prot     int
+	buf      []byte
+	offset   int64
+	modified bool
+}
+
+func OpenMappedFile(filename string, opMode OpMode) (*mappedFile, error) {
+	if e := opMode.verify(); e != nil {
+		return nil, errors.ErrorWithCause(e, "OpenMappedFile")
+	}
+
+	var flags int
+	var oflags int
+	var prot int
+	switch opMode {
+	case Read:
+		prot = syscall.MAP_PRIVATE
+		prot = syscall.PROT_READ
+		oflags = os.O_RDONLY
+	case Write:
+		flags = syscall.MAP_SHARED
+		prot = syscall.PROT_WRITE
+		oflags = os.O_RDWR
+	default:
+		panic(errors.Bug("OpenMappedFile: unsupported opMode: %s", opMode))
+	}
+
+	file, e := os.OpenFile(filename, oflags, system.FilePerm)
+	if e != nil {
+		return nil, e
+	}
+	// Note:close file on all errors after this point
+
+	finfo, e := file.Stat()
+	if e != nil {
+		file.Close()
+		return nil, e
+	}
+
+	var h header
+	mmf := &mappedFile{
+		header:   &h,
+		filename: filename,
+		opMode:   opMode,
+		file:     file,
+		finfo:    finfo,
+		prot:     prot,
+		flags:    flags,
+	}
+
+	var offset int64 = 0
+	if e := mmf.mmap(offset, int(finfo.Size()), false); e != nil {
+		file.Close()
+		return nil, e
+	}
+
+	return mmf, nil
+}
+
+// unmapAndClose first updates header timestamp and (re)writes
+// the header bytes (if opMode is Read & modified). If so, the
+// bool retval will reflect that.
+func (mmf *mappedFile) UnmapAndClose() (bool, error) {
+	// NOTE the write of header is here is very important
+	// REVU this should be done in AddObject (which is the only
+	// place that actually modifies the header! It does not belong here.
+	// TODO first try it in ref/mmap.go
+	fmt.Printf("debug - mappedFile.unamp -- IN\n")
+	var updated bool
+	if mmf.modified {
+		mmf.header.updated = time.Now().UnixNano()
+		mmf.header.encode(mmf.buf)
+		updated = true
+	}
+	if e := mmf.unmap(); e != nil {
+		return updated, e
+	}
+	if e := mmf.file.Close(); e != nil {
+		return updated, e
+	}
+	mmf.file = nil
+	return updated, nil
+}
+
+// if mmf.buf != nil, function first unmaps and then maps again at given
+// offset and for buf length specfieid -- effectively remap. Otherwise it
+// just a mapping.
+func (mmf *mappedFile) mmap(offset int64, length int, remap bool) error {
+	if mmf.buf != nil {
+		if !remap {
+			return errors.Bug("mappedFile.mmap: []buf not nil - remap: %t", remap)
+		}
+		if e := mmf.unmap(); e != nil {
+			return e
+		}
+	}
+	var fd = int(mmf.file.Fd())
+	buf, e := syscall.Mmap(fd, offset, length, mmf.prot, mmf.flags)
+	if e != nil {
+		return e
+	}
+	mmf.buf = buf
+	mmf.offset = offset
+	if !remap {
+		if e := mmf.header.decode(mmf.buf); e != nil {
+			if e0 := mmf.unmap(); e0 != nil {
+				return errors.Error("mappedFile.mmap: deocde-err:%s - unmap-err:%s", e, e0)
+			}
+			return e
+		}
+	}
+
+	return nil
+}
+
+func (mmf *mappedFile) unmap() error {
+	fmt.Printf("debug - mappedFile.unamp -- IN")
+	if mmf.buf == nil {
+		return errors.Bug("mappedFile.unmap: buf is nil")
+	}
+	if e := syscall.Munmap(mmf.buf); e != nil {
+		return errors.FaultWithCause(e, "mappedFile.unmap: %q", mmf.filename)
+	}
+	mmf.buf = nil
+	mmf.offset = 0
+	return nil
 }
 
 /// adhoc test /////////////////////////////////////////////////////////////////
@@ -181,7 +427,19 @@ func createTagmaps(tags ...string) error {
 }
 
 func readTagmap(tag string) error {
-	return errors.NotImplemented("readTagmap")
+	mmf, e := OpenTagmap(tag, Read)
+	if e != nil {
+		return e
+	}
+	fmt.Printf("% 02x\n", mmf)
+	updated, e := mmf.UnmapAndClose()
+	if e != nil {
+		return e
+	}
+	if updated {
+		return errors.Bug("UnmapAndClose() returned updated true")
+	}
+	return nil
 }
 
 func writeTagmap(tag string) error {
