833d822cca7e32af1f8b35ce7e8a800eb31a8d9d
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-26 08:58:16 -0400

    FIX BUG CreateTagmap writes to the correct repo file.

--- syslib/bitmap/tagmap/tagmaps.go
+++ syslib/bitmap/tagmap/tagmaps.go
@@ -31,6 +31,78 @@ import (
 // - tagmap manager
 //		- AND 2 or more tagmaps and get 'keyset'
 
+/// tagmaps prototype //////////////////////////////////////////////////////////
+
+const headerSize = 512
+
+type header struct {
+	ftype    uint64
+	crc64    uint64
+	created  int64
+	updated  int64
+	reserved [480]byte
+}
+
+func (h header) encode(buf []byte) error {
+	if len(buf) < headerSize {
+		return errors.Error("header.encode: insufficient buffer length: %d", len(buf))
+	}
+	*(*uint64)(unsafe.Pointer(&buf[0])) = h.ftype
+	*(*int64)(unsafe.Pointer(&buf[16])) = h.created
+	*(*int64)(unsafe.Pointer(&buf[24])) = h.updated
+
+	h.crc64 = digest.Checksum64(buf[16:])
+	*(*uint64)(unsafe.Pointer(&buf[8])) = h.crc64
+	return nil
+}
+
+// Returns the absolute path filename for the given tag.
+func TagFilename(tag string) string {
+	hash := fmt.Sprintf("%x.bitmap", digest.SumUint64([]byte(tag)))
+	path := filepath.Join(system.IndexTagmapsPath, hash[:2])
+	return filepath.Join(path, hash[2:])
+}
+
+// Creates the initial tagmap file for the given tag in the canonical
+// repo location. Tag names in gart are case-insensitive and the tag
+// (name) will always be converted to lower-case form.
+func CreateTagmap(tag string) error {
+
+	tag = strings.ToLower(tag)
+	filename := TagFilename(tag)
+	dir := filepath.Dir(filename)
+	if e := os.MkdirAll(dir, system.DirPerm); e != nil {
+		return errors.ErrorWithCause(e, "CreateTagmap: dir: %q", dir)
+	}
+
+	file, e := fs.OpenNewFile(filename, os.O_WRONLY|os.O_APPEND)
+	if e != nil {
+		return errors.ErrorWithCause(e, "CreateTagmap: tag: %q", tag)
+	}
+	defer file.Close()
+
+	var now = time.Now().UnixNano()
+	var h = &header{
+		ftype:   0x5807263e43839459,
+		created: now,
+		updated: now,
+	}
+
+	var buf [headerSize]byte
+	if e := h.encode(buf[:]); e != nil {
+		return e
+	}
+
+	_, e = file.Write(buf[:])
+	if e != nil {
+		return errors.ErrorWithCause(e, "createTagmap: tag: %s", tag)
+	}
+
+	return errors.NotImplemented("createTagmap - filename: %s", filename)
+}
+
+/// adhoc test /////////////////////////////////////////////////////////////////
+
 var option = struct {
 	op   string
 	tags string
@@ -81,13 +153,13 @@ op_verified:
 	var e error
 	switch option.op {
 	case "c":
-		e = createTagmaps(system.RepoPath, tagnames...)
+		e = createTagmaps(tagnames...)
 	case "r":
-		e = readTagmap(system.RepoPath, tagnames[0])
+		e = readTagmap(tagnames[0])
 	case "w":
-		e = writeTagmap(system.RepoPath, tagnames[0])
+		e = writeTagmap(tagnames[0])
 	case "q":
-		e = queryTagmaps(system.RepoPath, tagnames...)
+		e = queryTagmaps(tagnames...)
 	default:
 		exitOnError(errors.Bug("verified op is not known: %q", option.op))
 	}
@@ -99,84 +171,23 @@ op_verified:
 	os.Exit(0)
 }
 
-/// op prototypes //////////////////////////////////////////////////////////////
-
-func createTagmaps(repoDir string, tags ...string) error {
+func createTagmaps(tags ...string) error {
 	for _, tag := range tags {
-		if e := createTagmap(repoDir, tag); e != nil {
+		if e := CreateTagmap(tag); e != nil {
 			return e
 		}
 	}
 	return nil
 }
 
-const headerSize = 512
-
-type header struct {
-	ftype    uint64
-	crc64    uint64
-	created  int64
-	updated  int64
-	reserved [480]byte
-}
-
-func (h header) encode(buf []byte) error {
-	if len(buf) < headerSize {
-		return errors.Error("header.encode: insufficient buffer length: %d", len(buf))
-	}
-	*(*uint64)(unsafe.Pointer(&buf[0])) = h.ftype
-	*(*int64)(unsafe.Pointer(&buf[16])) = h.created
-	*(*int64)(unsafe.Pointer(&buf[24])) = h.updated
-
-	h.crc64 = digest.Checksum64(buf[16:])
-	*(*uint64)(unsafe.Pointer(&buf[8])) = h.crc64
-	return nil
-}
-
-func tagFilename(path, tag string) string {
-	name := fmt.Sprintf("%x.bitmap", digest.SumUint64([]byte(tag)))
-	filename := filepath.Join(path, name)
-	return filename
-}
-
-func createTagmap(repoDir string, tag string) error {
-
-	filename := tagFilename(repoDir, tag)
-
-	file, e := fs.OpenNewFile(filename, os.O_WRONLY|os.O_APPEND)
-	if e != nil {
-		return fmt.Errorf("oidx.CreateIndex: %s", e)
-	}
-	defer file.Close()
-
-	var now = time.Now().UnixNano()
-	var h = &header{
-		ftype:   0x5807263e43839459,
-		created: now,
-		updated: now,
-	}
-
-	var buf [headerSize]byte
-	if e := h.encode(buf[:]); e != nil {
-		return e
-	}
-
-	_, e = file.Write(buf[:])
-	if e != nil {
-		return errors.ErrorWithCause(e, "createTagmap: tag: %s", tag)
-	}
-
-	return errors.NotImplemented("createTagmap - filename: %s", filename)
-}
-
-func readTagmap(repoDir string, tag string) error {
+func readTagmap(tag string) error {
 	return errors.NotImplemented("readTagmap")
 }
 
-func writeTagmap(repoDir string, tag string) error {
+func writeTagmap(tag string) error {
 	return errors.NotImplemented("writeTagmap")
 }
 
-func queryTagmaps(repoDir string, tags ...string) error {
+func queryTagmaps(tags ...string) error {
 	return errors.NotImplemented("queryTagmaps")
 }
