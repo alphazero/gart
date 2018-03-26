a82f28e1532bcf6f73e25fbc286b5c27596a7646
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-26 04:46:33 -0400

    MOD errors is a sub-package of syslib

---
+++ syslib/errors/adhoc/errors-check.go
@@ -0,0 +1,24 @@
+// Doost!
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/alphazero/gart/syslib/errors"
+	"github.com/alphazero/gart/system"
+)
+
+func main() {
+	fmt.Printf("Salaam Samad Sultan of LOVE!\n")
+
+	fmt.Printf("%s\n", errors.ErrInvalidArg)
+	fmt.Printf("%s\n", system.BugInvalidOidBytesData)
+	fmt.Printf("%s\n", errors.BugInvalidArg)
+	fmt.Printf("%s\n", errors.NotImplemented("gart/syslib/errors/adhoc/main"))
+	fmt.Printf("%s\n", errors.Usage("unknown option %s", "foo"))
+
+	e := fmt.Errorf("not really an error")
+	msg := "Salaam Samad Sultan of LOVE"
+	fmt.Printf("%s\n", errors.ErrorWithCause(e, "adhoc.main: errors check - msg %s", msg))
+}
---
+++ syslib/errors/errors.go
@@ -0,0 +1,67 @@
+// Doost!
+
+package errors
+
+import (
+	"fmt"
+)
+
+/// defined generic errors /////////////////////////////////////////////////////
+
+var (
+	ErrNotImplemented = Error("function not implemented")
+	ErrInvalidArg     = Error("invalid argument")
+	ErrNilArg         = Error("nil input argument")
+)
+
+/// defined generic bugs ///////////////////////////////////////////////////////
+
+var (
+	// bugs are for internal use and assert checks.
+	BugInvalidArg = Bug("invalid input argument")
+	BugNilArg     = Bug("nil input argument")
+)
+
+/// defined generic faults /////////////////////////////////////////////////////
+
+var (
+	FaultNotImplemented = Fault("function not implemented")
+)
+
+/// err/bug uniform formatters /////////////////////////////////////////////////
+
+func NotImplemented(fmtstr string, a ...interface{}) error {
+	return fmterr("err", fmtstr+" is not implemented", a...)
+}
+
+func Usage(fmtstr string, a ...interface{}) error {
+	return fmterr("usage:", fmtstr, a...)
+}
+
+func Error(fmtstr string, a ...interface{}) error {
+	return fmterr("err", fmtstr, a...)
+}
+
+func Bug(fmtstr string, a ...interface{}) error {
+	return fmterr("bug", fmtstr, a...)
+}
+
+func Fault(fmtstr string, a ...interface{}) error {
+	return fmterr("fault", fmtstr, a...)
+}
+
+func ErrorWithCause(e error, fmtstr string, a ...interface{}) error {
+	return fmterr("err", fmtstr+" - cause: %v", append(a, e)...)
+}
+
+func BugWithCause(e error, fmtstr string, a ...interface{}) error {
+	return fmterr("bug", fmtstr+" - cause: %v", append(a, e)...)
+}
+
+func FaultWithCause(e error, fmtstr string, a ...interface{}) error {
+	return fmterr("fault", fmtstr+" - cause: %v", append(a, e)...)
+}
+
+func fmterr(what, fmtstr string, a ...interface{}) error {
+	return fmt.Errorf(what+" - "+fmtstr, a...)
+}
