27e3f5583bb5dda8d42b9dd72978665c6d27f8d4
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-26 08:57:17 -0400

    FIX MERGE conflict

--- syslib/errors/adhoc/errors-check.go
+++ syslib/errors/adhoc/errors-check.go
@@ -6,25 +6,17 @@ import (
 	"fmt"
 
 	"github.com/alphazero/gart/syslib/errors"
-<<<<<<< HEAD
 	"github.com/alphazero/gart/system"
-=======
->>>>>>> gart-2.0-bitmap
 )
 
 func main() {
 	fmt.Printf("Salaam Samad Sultan of LOVE!\n")
 
 	fmt.Printf("%s\n", errors.ErrInvalidArg)
-<<<<<<< HEAD
 	fmt.Printf("%s\n", system.BugInvalidOidBytesData)
 	fmt.Printf("%s\n", errors.BugInvalidArg)
 	fmt.Printf("%s\n", errors.NotImplemented("gart/syslib/errors/adhoc/main"))
 	fmt.Printf("%s\n", errors.Usage("unknown option %s", "foo"))
-=======
-	fmt.Printf("%s\n", errors.BugInvalidOidBytesData)
-	fmt.Printf("%s\n", errors.BugInvalidArg)
->>>>>>> gart-2.0-bitmap
 
 	e := fmt.Errorf("not really an error")
 	msg := "Salaam Samad Sultan of LOVE"
