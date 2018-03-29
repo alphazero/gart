d689bb2f674804e8a9303af61c0bfbb055da58fc
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-03-29 17:46:50 -0400

    ADD WIP NO-TEST initial sketch of indexManager API. COMPILES.

--- index/index.go
+++ index/index.go
@@ -16,23 +16,78 @@ var (
 	ErrObjectIndexClosed   = errors.Error("object index is closeed")
 )
 
-/// types //////////////////////////////////////////////////////////////////////
+/// index manager //////////////////////////////////////////////////////////////
 
 // TODO
 // significant todo here is nailing down the 'query' for selecting all object ids
 // where tags are 'set'. Simple option is to for now just do a specific function for
 // querying OIDs for a given set of tags to be ANDed.
 
-type indexManager struct{}
+// REVU be consistent with type name (exported or pkg prv.)
+type indexManager struct {
+	opMode  OpMode
+	oidx    *oidxFile
+	tagmaps map[string]*Tagmap
+}
 
-func OpenIndexManager(repoDir string) (*indexManager, error) {
-	var idxmgr = &indexManager{}
+// index.Initialize creates the various necessary indexes for gart in the
+// canonical system.RepoDir.
+func InitializeRepo() error {
+	if e := createObjectIndex(); e != nil {
+		return errors.ErrorWithCause(e,
+			"index.InitializeRepo: error creating objects index")
+	}
 
-	// REVU for now. probably needs to have a 'tag manager' instance
-	// and check various required files.
-	// TODO make sure if we are -not- passing repoDir around anymore
-	// to also remove it from this
+	// REVU what else?
+	//		Tagmaps are per tag. Should it create Tagdict?
+	//		TODO tagmaps for 'systemic' tags could be done here.
+	//		Card files are per object. Is there to be a Cards object?
+
+	return nil
+}
+
+// REVU: turns out OpMode is not just for objects.go.
+func OpenIndexManager(opMode OpMode) (*indexManager, error) {
+
+	oidx, e := openObjectIndex(opMode)
+	if e != nil {
+		return nil, e
+	}
+
+	var idxmgr = &indexManager{
+		opMode:  opMode,
+		oidx:    oidx,
+		tagmaps: make(map[string]*Tagmap),
+	}
+
 	return idxmgr, nil
+}
+
+// Preloads the associated Tagmaps for the tags. This doesn't necessary mean
+// that we can't query using tags not specified here. (REVU it shouldn't.)
+//
+// Intended usecase is for a gart-tool to say:
+//
+//    index.OpenIndexManager(OpMode.Read).UsingTags(tags...)
+//
+func (idx *indexManager) UsingTags(tags ...string) (*indexManager, error) {
+	for i, tag := range tags {
+		if _, ok := idx.tagmaps[tag]; ok {
+			continue // already loaded
+		}
+		tagmap, e := LoadTagmap(tag, false)
+		if e != nil {
+			return idx, errors.ErrorWithCause(e, "index.UsingTags: tag[%d]:%q", i, tag)
+		}
+		idx.tagmaps[tag] = tagmap
+	}
+	return idx, nil
+}
+
+// Indexes the object identified by the Oid.
+// REVU see gart-add in /1.0/ for refresh on systemics..
+func (idx *indexManager) IndexObject(oid *system.Oid, tags ...string) (bool, error) {
+	return false, errors.NotImplemented("indexManager.IndexObject")
 }
 
 // REVU need to revisit system/types.go
