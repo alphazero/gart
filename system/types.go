a117c1fc74a13405a18a0165cb02d388a1e90a48
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-04-01 04:28:48 -0400

    ADD NO-TEST gart object types are defined by system.Otype

--- system/types.go
+++ system/types.go
@@ -4,7 +4,6 @@ package system
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/alphazero/gart/syslib/errors"
 )
@@ -12,83 +11,48 @@ import (
 /// Runtime Context ////////////////////////////////////////////////////////////
 
 // Context is the system runtime context.
-type Context struct {
-	TagManager
-	Index
+type Context struct{} // REVU
+
+/// Object Type ////////////////////////////////////////////////////////////////
+
+type Otype byte
+
+const (
+	_ Otype = iota
+	Data
+	Text
+	File
+	URI
+	URL
+)
+
+func (v Otype) Verify() error {
+	switch v {
+	case Data:
+	case Text:
+	case File:
+	case URI:
+	case URL:
+	default:
+		return errors.Bug("system.Otype: unknown object type - %d", v)
+	}
+	return nil
 }
 
-/// Object tagging /////////////////////////////////////////////////////////////
-
-// A Tag is a user defined attribute associated with archived content. Tags
-// unique (but case insensitive) user defined names and system assigned ids.
-type Tag interface {
-	// Returns the user defined name of the Tag.
-	Name() string
-	// Returns the non-zero unique system id assigned to this Tag.
-	Id() int
-	// Returns the number of objects tagged with this Tag.
-	Refcnt() int
-}
-
-// TagManager defines the interface to a persistent store of user defined
-// Tags, allowing for the addtion, lookup, and updating of Tag reference counts.
-type TagManager interface {
-	// Returns the number of Tags in the dictionary.
-	Size() int
-
-	// Adds a new tag. Tag names are case-insensitive, non-zerolen, and at most
-	// MaxTagNameSize. An ErrInvalidTagName error is returned
-	//
-	// Returns (true, id, nil) if tag was added, otherwise (false, id, nil)
-	// with the id of the existing tag.
-	//
-	// Returns (false, 0, ErrInvalidArg) if the size requirement is not
-	// met. Any other error returned is indicating of a bug or fault.
-	Add(name string) (added bool, id int, err error)
-
-	// Increments the named tag's refcnt and returns the new refcnt.
-	// Returns ErrTagNotFound error if tag does not exist. Any other error
-	// is indicative of a bug or fault.
-	IncrRefcnt(name string) (refcnt int, id int, err error)
-
-	// Returns ids of selected tags. These are used to build index bitmaps.
-	// notDefined is never nil. If not empty, it contains all
-	// tag names that are not defined.
-	SelectTags(names []string) (ids []int, notDefined []string)
-
-	// Syncs the tagmap file. IFF the in-mem model has been modified
-	Sync() (ok bool, e error)
-
-	// List tags
-	Tags() []Tag
-}
-
-/// Object indexing ////////////////////////////////////////////////////////////
-
-// Card defines the public attributes of the index card of an archived object.
-type IndexCard interface {
-	CreatedOn() time.Time // unix seconds precision
-	UpdatedOn() time.Time // unix seconds precision
-	Revision() int        // 0 indicates new card
-	ObjectId() *Oid       //
-	Tags() []string       //
-	Systemic() []string   //
-	Paths() []string      // REVU len(card.Paths()) > 1 => dup files
-
-	// REVU see 1.0/index.go#indexCard interface. For providers.
-	//	IndexKey() uint64     // 0 indicates card is not indexed
-
-	// REVU move to Index ?
-	//	AddPath(fpath string) (bool, error)
-	//	RemovePath(fpath string) (bool, error)
-	//	SetTags(cpm bitmap.Bitmap) (bool, error)
-	//	SetSystemics(cpm bitmap.Bitmap) (bool, error)
-	//	Save() (bool, error)
-	//	DebugStr() string
-}
-
-type Index interface {
-	/* AddObject, Lookup, .. ? */
+func (v Otype) String() string {
+	switch v {
+	case Data:
+		return "data"
+	case Text:
+		return "text"
+	case File:
+		return "file"
+	case URI:
+		return "uri"
+	case URL:
+		return "url"
+	}
+	panic(errors.Bug("Otype.String: unknown type - %d", v))
 }
 
 /// Object Identity ////////////////////////////////////////////////////////////
@@ -139,3 +103,76 @@ func (oid *Oid) Fingerprint() string {
 	return s[:7] + ".." + s[len(s)-4:]
 }
 func (oid *Oid) String() string { return fmt.Sprintf("%x", oid.dat) }
+
+/// Object tagging /////////////////////////////////////////////////////////////
+
+// A Tag is a user defined attribute associated with archived content. Tags
+// unique (but case insensitive) user defined names and system assigned ids.
+type Tag interface {
+	// Returns the user defined name of the Tag.
+	Name() string
+	// Returns the non-zero unique system id assigned to this Tag.
+	Id() int
+	// Returns the number of objects tagged with this Tag.
+	Refcnt() int
+}
+
+// TagManager defines the interface to a persistent store of user defined
+// Tags, allowing for the addtion, lookup, and updating of Tag reference counts.
+type TagManager interface {
+	// Returns the number of Tags in the dictionary.
+	Size() int
+
+	// Adds a new tag. Tag names are case-insensitive, non-zerolen, and at most
+	// MaxTagNameSize. An ErrInvalidTagName error is returned
+	//
+	// Returns (true, id, nil) if tag was added, otherwise (false, id, nil)
+	// with the id of the existing tag.
+	//
+	// Returns (false, 0, ErrInvalidArg) if the size requirement is not
+	// met. Any other error returned is indicating of a bug or fault.
+	Add(name string) (added bool, id int, err error)
+
+	// Increments the named tag's refcnt and returns the new refcnt.
+	// Returns ErrTagNotFound error if tag does not exist. Any other error
+	// is indicative of a bug or fault.
+	IncrRefcnt(name string) (refcnt int, id int, err error)
+
+	// Returns ids of selected tags. These are used to build index bitmaps.
+	// notDefined is never nil. If not empty, it contains all
+	// tag names that are not defined.
+	SelectTags(names []string) (ids []int, notDefined []string)
+
+	// Syncs the tagmap file. IFF the in-mem model has been modified
+	Sync() (ok bool, e error)
+
+	// List tags
+	Tags() []Tag
+}
+
+/// Object indexing ////////////////////////////////////////////////////////////
+
+/* XXX deprecated pending REVU
+
+// Card defines the public attributes of the index card of an archived object.
+type IndexCard interface {
+	CreatedOn() time.Time // unix seconds precision
+	UpdatedOn() time.Time // unix seconds precision
+	Revision() int        // 0 indicates new card
+	ObjectId() *Oid       //
+	Tags() []string       //
+	Systemic() []string   //
+	Paths() []string      // REVU len(card.Paths()) > 1 => dup files
+
+	// REVU see 1.0/index.go#indexCard interface. For providers.
+	//	IndexKey() uint64     // 0 indicates card is not indexed
+
+	// REVU move to Index ?
+	//	AddPath(fpath string) (bool, error)
+	//	RemovePath(fpath string) (bool, error)
+	//	SetTags(cpm bitmap.Bitmap) (bool, error)
+	//	SetSystemics(cpm bitmap.Bitmap) (bool, error)
+	//	Save() (bool, error)
+	//	DebugStr() string
+}
+*/
