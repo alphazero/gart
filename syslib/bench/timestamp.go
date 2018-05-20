a9edbf6777fab6410dca13307bdb7d947c849ac8
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2018-05-20 10:21:33 -0400

    ADD time/op measure for bench/timestamp

--- syslib/bench/timestamp.go
+++ syslib/bench/timestamp.go
@@ -11,8 +11,23 @@ type timestamp int64
 
 func NewTimestamp() timestamp { return timestamp(time.Now().UnixNano()) }
 
-func (t0 *timestamp) Mark(s string) {
-	now := time.Now().UnixNano()
-	fmt.Printf(">>> step:%s - dt:%s\n", s, time.Duration(now-int64(*t0)))
+func (t0 *timestamp) mark() int64 {
+	var now = time.Now().UnixNano()
+	var dt = now - int64(*t0)
 	*t0 = timestamp(now)
+	return dt
+}
+
+func (t0 *timestamp) Mark(s string) time.Duration {
+	dt := t0.mark()
+	fmt.Printf(">>> step:%s - dt:%s\n", s, dt)
+	return time.Duration(dt)
+}
+
+func (t0 *timestamp) MarkN(s string, ops int) (dt, dtpo time.Duration) {
+	d := t0.mark()
+	dt = time.Duration(d)
+	dtpo = time.Duration(int64(float64(d) / float64(ops)))
+	fmt.Printf(">>> step:%s - dt:%s t/op:%s\n", s, dt, dtpo)
+	return
 }
