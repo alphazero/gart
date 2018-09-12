e80f4be3ae16e359544c58d39f15f61aacd94b53
Joubin Muhammad Houshyar <jhoushyar@gmail.com>
2018-09-12 08:51:27 -0400

    MOD minor cleanup of bench.timestamp type.

--- syslib/bench/timestamp.go
+++ syslib/bench/timestamp.go
@@ -7,27 +7,25 @@ import (
 	"time"
 )
 
-type timestamp int64
+type timestamp time.Time
 
-func NewTimestamp() timestamp { return timestamp(time.Now().UnixNano()) }
+func NewTimestamp() timestamp { return timestamp(time.Now()) }
 
-func (t0 *timestamp) mark() int64 {
-	var now = time.Now().UnixNano()
-	var dt = now - int64(*t0)
-	*t0 = timestamp(now)
+func (t0 *timestamp) mark() time.Duration {
+	dt := time.Since(time.Time(*t0))
+	*t0 = timestamp(time.Now())
 	return dt
 }
 
 func (t0 *timestamp) Mark(s string) time.Duration {
-	dt := time.Duration(t0.mark())
+	dt := t0.mark()
 	fmt.Printf("time-mark: %s - dt:%s\n", s, dt)
 	return dt
 }
 
 func (t0 *timestamp) MarkN(s string, ops int) (dt, dtpo time.Duration) {
-	d := t0.mark()
-	dt = time.Duration(d)
-	dtpo = time.Duration(int64(float64(d) / float64(ops)))
+	dt = t0.mark()
+	dtpo = time.Duration(int64(float64(dt) / float64(ops)))
 	fmt.Printf("time-mark: %s - dt:%s t/op:%s\n", s, dt, dtpo)
 	return
 }
